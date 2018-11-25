34a1893c20bb7353496ab6faefd1777267d5a22d
Jody Almaida Putra <jody.almaida@gmail.com>
2018-11-25 10:35:43 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,7 @@ Route::get('/jody-almaida', function(){
 	return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
-
-// 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
